0f0560a9a7c1669ef21fd6aa3e977795e30bbce2
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-04-01 02:55:38 +0200

    react rounting stup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/'
+import Survey from './pages/Survey/'
+import Header from './components/Header'
+import Error from './components/Error'
+// On ajoute nos composants
+// import ClientForm from './components/ClientForm'
+// import FreelanceForm from './components/FreelanceForm'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
