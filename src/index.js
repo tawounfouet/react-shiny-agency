50d587b8180a2058bce0940b6ed1197b29d0c632
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-04-17 15:22:34 +0200

    rajout des pages freelances et results

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import Home from './pages/Home/'
 import Survey from './pages/Survey/'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+
+
 import Header from './components/Header'
 import Error from './components/Error'
+import FreelanceForm from './components/FreelanceForm'
 // On ajoute nos composants
-// import ClientForm from './components/ClientForm'
+import ClientForm from './components/ClientForm'
 // import FreelanceForm from './components/FreelanceForm'
 
 
@@ -17,7 +23,13 @@ ReactDOM.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/survey" element={<Survey />} /> */}
         <Route path="/survey/:questionNumber" element={<Survey />} />
+        { /* Nous imbriquons nos composants dans survey */}
+          <Route path="client" element={<ClientForm />} />
+          <Route path="freelance" element={<FreelanceForm />} />
+          <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,3 +1,5 @@
+import { Outlet, Link } from 'react-router-dom'
+
 import { useParams } from 'react-router-dom'
  
 function Survey() {
@@ -7,6 +9,9 @@ function Survey() {
         <div>
             <h1>Questionnaire 🧮</h1>
             <h2>Question {questionNumber}</h2>
+            <Link to="client">Questionnaire Client</Link>
+            <Link to="freelance">Questionnaire Freelance</Link>
+            <Outlet />
         </div>
     )
 }
