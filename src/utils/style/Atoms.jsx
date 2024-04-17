d9d2b9dee3e3aea4858a4cb89f80e30e6a104e48
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-04-17 17:13:56 +0200

    final update before deployement

--- src/utils/style/Atoms.jsx
+++ src/utils/style/Atoms.jsx
@@ -34,3 +34,20 @@ export const StyledLink = styled(Link)`
     border-radius: 30px; 
     background-color: ${colors.primary};`}
 `
+
+// export const StyledLink = styled(Link)`
+//   padding: 10px 15px;
+//   color: ${({ $theme }) => ($theme === 'light' ? '#8186a0' : '#ffffff')};
+//   text-decoration: none;
+//   font-size: 18px;
+//   text-align: center;
+//   ${(props) =>
+//     props.$isFullLink &&
+//     `color: white; 
+//     border-radius: 30px; 
+//     background-color: ${colors.primary};`}
+    
+//   ${({ $theme }) => $theme === 'light' && `
+//     color: #8186a0;
+//   `}
+// `;
